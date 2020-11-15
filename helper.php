7b5178c94d985730c7602576fa39f4f639615bb0
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2020-11-15 19:04:44 +0200

    Add a helper file

---
+++ helper.php
@@ -0,0 +1,12 @@
+<?php 
+
+use App\Models\Setting;
+
+if (! function_exists('setting')) {
+    function setting(string $key) : string 
+    {
+        $setting = Setting::where('key', $key)->first();
+    
+        return $setting ? $setting->value : '';
+    }
+}
