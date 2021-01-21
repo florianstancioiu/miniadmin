3d68007ce12c093662d4232effb407cf0a77f9e9
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2021-01-21 16:09:38 +0200

    Indent and add error messages to register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,86 +3,104 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Registration Page</title>
+  <title>Register | {{ setting('site-title') }}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
 <body class="hold-transition register-page">
-<div class="register-box">
-  <div class="register-logo">
-    <a href="{{ url('/register') }}">
-        {{ setting('site-title') }}
-    </a>
-  </div>
+    <div class="register-box">
+        <div class="register-logo">
+            <a href="{{ url('/register') }}">
+                {{ setting('site-title') }}
+            </a>
+        </div>
 
-  <div class="card">
-    <div class="card-body register-card-body">
-      <p class="login-box-msg">Register a new membership</p>
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="login-box-msg">Register a new membership</p>
 
-      <form action="{{ route('register')}}" method="post">
-        @csrf
+                <form action="{{ route('register')}}" method="post">
+                    @csrf
 
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" name="first_name" placeholder="First name">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+                    @error('first_name')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="first_name" placeholder="First name">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    @error('last_name')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="last_name" placeholder="Last name">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    @error('email')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+
+                    <div class="input-group mb-3">
+                        <input type="email" class="form-control" name="email" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    @error('password')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    @error('password_confirmation')
+                        <p class="text-danger">{{ $message }}</p>
+                    @enderror
+
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password_confirmation" placeholder="Retype password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">Register</button>
+                        </div>
+                    </div>
+
+                </form>
+                <a href="{{ url('/login') }}" class="text-center">I already have a membership</a>
             </div>
-          </div>
-        </div>
-
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" name="last_name" placeholder="Last name">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
-        </div>
-
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" name="email" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password_confirmation" placeholder="Retype password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
-         </div>
-
-        </div>
-
-      </form>
-      <a href="{{ url('/login') }}" class="text-center">I already have a membership</a>
+        </div><!-- /.card -->
     </div>
-    <!-- /.form-box -->
-  </div><!-- /.card -->
-</div>
-<!-- /.register-box -->
 
-<script src="{{ asset('js/admin.js') }}"></script>
-
+    <script src="{{ asset('js/admin.js') }}"></script>
 </body>
 </html>
