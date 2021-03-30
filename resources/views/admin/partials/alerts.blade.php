d7bae47a45b4e842d2faf4d988537ea987e4affb
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2021-03-30 14:14:16 +0300

    Use dissmissable alerts

--- resources/views/admin/partials/alerts.blade.php
+++ resources/views/admin/partials/alerts.blade.php
@@ -1,23 +1,23 @@
 @if($errors->all())
-    <div class="card card-danger">
-        <div class="card-header">
-            <h3 class="card-title">{{ __('partials.failure') }}!</h3>
-        </div>
-        <div class="card-body">
-            @foreach($errors->all() as $error)
-                <p>{{ $error }}</p>
-            @endforeach
-        </div>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <h4 class="alert-heading">{{ __('partials.failure') }}!</h4>
+        <hr>
+        @foreach($errors->all() as $error)
+            <p class="mb-0">{{ $error }}</p>
+        @endforeach
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 
 @if(session()->has('message'))
-    <div class="card card-success">
-        <div class="card-header">
-            <h3 class="card-title">{{ __('partials.success') }}!</h3>
-        </div>
-        <div class="card-body">
-            <p>{{ session()->get('message') }}</p>
-        </div>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <h4 class="alert-heading">{{ __('partials.success') }}!</h4>
+        <hr>
+        <p class="mb-0">{{ session()->get('message') }}</p>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
