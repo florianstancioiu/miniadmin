ccfb0b9dd15356ec33777f107c74c1d3feed4117
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2021-12-07 15:54:47 +0200

    Make the admin and client js output files smaller

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,3 +1,3 @@
-require('./bootstrap');
 require('./admin/libraries');
+require('./admin/adminlte/AdminLTE');
 require('./admin/general');
--- resources/js/admin/libraries.js
+++ resources/js/admin/libraries.js
@@ -1,11 +1,5 @@
-require('jquery/dist/jquery.min');
-require('jquery-ui-dist/jquery-ui.min');
-
-// Resolve conflict in jQuery UI tooltip with Bootstrap tooltip
-$.widget.bridge('uibutton', $.ui.button);
-
+window.$ = window.jQuery = require('jquery/dist/jquery.min');
 require('bootstrap/dist/js/bootstrap.bundle.min');
 require('jquery-knob-chif/dist/jquery.knob.min');
 require('overlayscrollbars/js/OverlayScrollbars.min');
-require('simplemde/dist/simplemde.min')
-require('./adminlte');
+require('simplemde/dist/simplemde.min');
